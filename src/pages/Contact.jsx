f874f636864498bb2d23b0f1546d1d6ded15086e
Aheli001 <ahelinandy687@gmail.com>
2025-03-06 18:03:05 +0530

    add contact form with email functionality and loading state

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,92 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const Contact = () => {
+  const formRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [form, setForm] = useState(
+    {
+      name: '',
+      email: '',
+      message: ''
+    }
+  );
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    setIsLoading(true);
+  }
+  const handleFocus = () => { }
+
+  const handleBlur = () => { }
+
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text blue-gradient_text'>Get in Touch</h1>
+
+        <form className='w-full flex flex-col gap-7 mt-14' onSubmit={handleSubmit}>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='Enter your name'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='Enter your email'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              className='textarea resize-none'
+              rows={5}
+              placeholder='Let me know how I can help you!'
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type='submit'
+            className='btn'
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            disabled={isLoading}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+
+    </section>
   )
 }
 
